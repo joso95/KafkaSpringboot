1a3597499c7c064a0a7970ab0d6bc114801c865c
Jorgen S <j-solum@live.com>
2023-11-02 21:14:06 +0100

    Update KafkaConsumer.java
    finish consumer

--- src/main/java/com/example/kafka/kafka/demo/consumer/KafkaConsumer.java
+++ src/main/java/com/example/kafka/kafka/demo/consumer/KafkaConsumer.java
@@ -1,12 +1,23 @@
 package com.example.kafka.kafka.demo.consumer;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
 
+@Service
 public class KafkaConsumer {
+
+    Logger log = LoggerFactory.getLogger(KafkaConsumer.class);
 
     @KafkaListener(topics = "CustomerHistory", groupId = "foo")
     public void listenGroupFoo(String message) {
         System.out.println("Received Message in group foo: " + message);
+    }
+
+    @KafkaListener(topics = "CustomerHistory")
+    public void consume(String msg){
+        log.info("consuming: {}", msg);
     }
 
 }
